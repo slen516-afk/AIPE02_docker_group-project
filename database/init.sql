aae6e580ec51041a3989523cea651b25be182f3c
Tim <orange70297@gmail.com>
2025-12-25 02:33:02 +0800

    data_insert_into_db(volume)

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE IF NOT EXISTS project_db;
 USE project_db;
+
+CREATE TABLE IF NOT EXISTS temp_raw_data (
+    title VARCHAR(255),
+    telecommuting TINYINT,
+    has_company_logo TINYINT,
+    has_questions TINYINT,
+    employment_type VARCHAR(100),
+    fraudulent TINYINT,
+    in_balanced_dataset TINYINT,
+    country VARCHAR(100),
+    industry_group VARCHAR(100),
+    edu_level VARCHAR(100)
+);
